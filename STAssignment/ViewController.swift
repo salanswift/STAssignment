92be33732325c0ae386648b21402bda56c5a0cbb
Arsalan Akhtar <tarsalan@hla.hongleong.com.my>
2019-11-21 21:53:18 +0800

    Initial Commit

---
+++ STAssignment/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  STAssignment
+//
+//  Created by Arsalan Akhtar on 11/21/19.
+//  Copyright © 2019 Arsalan Akhtar. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view.
+	}
+
+
+}
+
